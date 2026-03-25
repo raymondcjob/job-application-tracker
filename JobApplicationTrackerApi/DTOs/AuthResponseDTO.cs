cc01d5d68188115aaa76214d1a751874ed5eee7c
raymondc <raymondcjob@gmail.com>
2026-03-25 15:13:49 -0700

    created auth dtos for register, login, and token response

---
+++ JobApplicationTrackerApi/DTOs/AuthResponseDTO.cs
@@ -0,0 +1,8 @@
+namespace JobApplicationTrackerApi.DTOs;
+
+public class AuthResponseDto
+{
+    public string Token { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+}
